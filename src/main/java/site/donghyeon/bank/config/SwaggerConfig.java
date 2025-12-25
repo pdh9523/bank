929c02b4bb894e458f5fbb35bbb78874096b20a4
박동현 <jinee9523@gmail.com>
2025-12-26 00:19:40 +0900

    feat: swagger 설정

---
+++ src/main/java/site/donghyeon/bank/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package site.donghyeon.bank.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("bank")
+                        .description("송금 서비스 API")
+                        .version("v.0.0.1")
+                );
+    }
+}
