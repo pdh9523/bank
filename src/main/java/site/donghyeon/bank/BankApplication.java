5f09f2367665a8e70f5848b1202afaf98c9cdf8d
박동현 <jinee9523@gmail.com>
2025-12-26 00:12:35 +0900

    feat: spring boot 초기화

---
+++ src/main/java/site/donghyeon/bank/BankApplication.java
@@ -0,0 +1,13 @@
+package site.donghyeon.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplication.class, args);
+	}
+
+}
