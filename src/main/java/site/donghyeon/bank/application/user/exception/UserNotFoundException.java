b9137b930e74ced3e79b5771899173ad09c263e8
박동현 <jinee9523@gmail.com>
2025-12-29 14:11:59 +0900

    chore: 오타 수정

--- src/main/java/site/donghyeon/bank/application/user/exception/UserNotFoundException.java
+++ src/main/java/site/donghyeon/bank/application/user/exception/UserNotFoundException.java
@@ -4,6 +4,6 @@ import site.donghyeon.bank.common.exception.ApplicationException;
 
 public class UserNotFoundException extends ApplicationException {
     public UserNotFoundException(String type, Object query) {
-        super("Account not found, %s: %s ".formatted(type, query));
+        super("User not found, %s: %s ".formatted(type, query));
     }
 }
